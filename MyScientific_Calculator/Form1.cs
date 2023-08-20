8b041fd3d3b336ab78b0d8564821b5a1b081f7b6
GYoung <ytok1108@aol.com>
2023-08-20 20:09:40 +0900

    remove comma

--- MyScientific_Calculator/Form1.cs
+++ MyScientific_Calculator/Form1.cs
@@ -44,49 +44,50 @@ namespace MyScientific_Calculator
                 else
                 {
                     txtResult.Text = txtResult.Text + num.Text;
+                }
 
                     // Add a comma every three digits
-                    string text = txtResult.Text;
-                    int index = 0;
-                    while (index < text.Length)
-                    {
-                        if (index > 0 && text[index - 1] != ',' && index % 3 == 0)
-                        {
-                            txtResult.Text = txtResult.Text.Insert(index, ",");
-                        }
-                        index++;
-                    }
-                    // ConvertDecimalToHexadecimal();
-                    // ConvertDecimalToOctal();
-                    // ConvertDecimalToBinary();
+                    //string text = txtResult.Text;
+                    //int index = 0;
+                    ////while (index < text.Length)
+                    //{
+                    //    if (index > 0 && text[index - 1] != ',' && index % 3 == 0)
+                    //    {
+                    //        txtResult.Text = txtResult.Text.Insert(index, ",");
+                    //    }
+                    //    index++;
+                    //}
+                    ConvertDecimalToHexadecimal();
+                    ConvertDecimalToOctal();
+                    ConvertDecimalToBinary();
                 }
             }
 
-            //            private void ConvertDecimalToHexadecimal()
-            //            {
-            //                string text = txtResult.Text;
-            //                text = text.Replace(",", "");
-            //                txtResult.Text = text;
-            //                int decimalNumber = Convert.ToInt32(text);
-            //                string hexadecimalNumber = Convert.ToString(decimalNumber, 16);
-            //                txtResultHex.Text = hexadecimalNumber;
-            //            }
-            //            private void ConvertDecimalToOctal()
-            //            {
-            //                string text = txtResult.Text;
-            //                text = text.Replace(",", "");
-            //                int decimalNumber = Convert.ToInt32(text);
-            //                string octalNumber = Convert.ToString(decimalNumber, 8);
-            //                txtResultOct.Text = octalNumber;
-            //            }
-            //            private void ConvertDecimalToBinary()
-            //            {
-            //                string text = txtResult.Text;
-            //                text = text.Replace(",", "");
-            //                int decimalNumber = Convert.ToInt32(text);
-            //                string binaryNumber = Convert.ToString(decimalNumber, 2);
-            //                txtResultBinary.Text = binaryNumber;
-        }
+            private void ConvertDecimalToHexadecimal()
+            {
+                string text = txtResult.Text;
+                text = text.Replace(",", "");
+                txtResult.Text = text;
+                int decimalNumber = Convert.ToInt32(text);
+                string hexadecimalNumber = Convert.ToString(decimalNumber, 16);
+                txtResultHex.Text = hexadecimalNumber;
+            }
+            private void ConvertDecimalToOctal()
+            {
+                string text = txtResult.Text;
+                text = text.Replace(",", "");
+                int decimalNumber = Convert.ToInt32(text);
+                string octalNumber = Convert.ToString(decimalNumber, 8);
+                txtResultOct.Text = octalNumber;
+            }
+            private void ConvertDecimalToBinary()
+            {
+                string text = txtResult.Text;
+                text = text.Replace(",", "");
+                int decimalNumber = Convert.ToInt32(text);
+                string binaryNumber = Convert.ToString(decimalNumber, 2);
+                txtResultBinary.Text = binaryNumber;
+            }
 
-    }
+        }
 }

289b93cc626ccd002963a3c1d10d5c9d46787e61
GYoung <ytok1108@aol.com>
2023-08-20 17:37:06 +0900

    Add project files.

---
+++ MyScientific_Calculator/Form1.Designer.cs
@@ -0,0 +1,416 @@
+﻿namespace MyScientific_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtResult = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            button9 = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
+            button13 = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            btn0 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("BatangChe", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 120);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 19);
+            label1.TabIndex = 0;
+            label1.Text = "10_Deci";
+            label1.Click += label1_Click;
+            // 
+            // txtResult
+            // 
+            txtResult.Font = new Font("Malgun Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtResult.Location = new Point(104, 108);
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(510, 35);
+            txtResult.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("BatangChe", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(23, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(75, 19);
+            label3.TabIndex = 3;
+            label3.Text = "16_Hex";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("BatangChe", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(34, 41);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 19);
+            label2.TabIndex = 4;
+            label2.Text = "8_Oct";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("BatangChe", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(23, 6);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 19);
+            label4.TabIndex = 5;
+            label4.Text = "2_Bina";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Malgun Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(104, 69);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(510, 33);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Malgun Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(104, 2);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(510, 29);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Malgun Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(104, 37);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(510, 29);
+            textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(23, 149);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 66);
+            button1.TabIndex = 9;
+            button1.Text = "⌫";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(104, 149);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 66);
+            button2.TabIndex = 10;
+            button2.Text = "CE";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(185, 149);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 66);
+            button3.TabIndex = 11;
+            button3.Text = "C";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(266, 149);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 66);
+            button4.TabIndex = 12;
+            button4.Text = "±";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(266, 221);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 66);
+            button5.TabIndex = 16;
+            button5.Text = "+";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            btn9.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btn9.Location = new Point(185, 221);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(75, 66);
+            btn9.TabIndex = 15;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += EnterNumbers;
+            // 
+            // btn8
+            // 
+            btn8.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btn8.Location = new Point(104, 221);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(75, 66);
+            btn8.TabIndex = 14;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += EnterNumbers;
+            // 
+            // btn7
+            // 
+            btn7.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btn7.Location = new Point(23, 221);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(75, 66);
+            btn7.TabIndex = 13;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += EnterNumbers;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button9.Location = new Point(266, 365);
+            button9.Name = "button9";
+            button9.Size = new Size(75, 66);
+            button9.TabIndex = 24;
+            button9.Text = "*";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btn3.Location = new Point(185, 365);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(75, 66);
+            btn3.TabIndex = 23;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += EnterNumbers;
+            // 
+            // btn2
+            // 
+            btn2.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btn2.Location = new Point(104, 365);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(75, 66);
+            btn2.TabIndex = 22;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += EnterNumbers;
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btn1.Location = new Point(23, 365);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(75, 66);
+            btn1.TabIndex = 21;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += EnterNumbers;
+            // 
+            // button13
+            // 
+            button13.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button13.Location = new Point(266, 293);
+            button13.Name = "button13";
+            button13.Size = new Size(75, 66);
+            button13.TabIndex = 20;
+            button13.Text = "-";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            btn6.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btn6.Location = new Point(185, 293);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(75, 66);
+            btn6.TabIndex = 19;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += EnterNumbers;
+            // 
+            // btn5
+            // 
+            btn5.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btn5.Location = new Point(104, 293);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(75, 66);
+            btn5.TabIndex = 18;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += EnterNumbers;
+            // 
+            // btn4
+            // 
+            btn4.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btn4.Location = new Point(23, 293);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(75, 66);
+            btn4.TabIndex = 17;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += EnterNumbers;
+            // 
+            // button17
+            // 
+            button17.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button17.Location = new Point(266, 437);
+            button17.Name = "button17";
+            button17.Size = new Size(75, 66);
+            button17.TabIndex = 28;
+            button17.Text = "/";
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            button18.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button18.Location = new Point(185, 437);
+            button18.Name = "button18";
+            button18.Size = new Size(75, 66);
+            button18.TabIndex = 27;
+            button18.Text = "=";
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            button19.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button19.Location = new Point(104, 437);
+            button19.Name = "button19";
+            button19.Size = new Size(75, 66);
+            button19.TabIndex = 26;
+            button19.Text = ".";
+            button19.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            btn0.Font = new Font("DotumChe", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btn0.Location = new Point(23, 437);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(75, 66);
+            btn0.TabIndex = 25;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += EnterNumbers;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(862, 511);
+            Controls.Add(button17);
+            Controls.Add(button18);
+            Controls.Add(button19);
+            Controls.Add(btn0);
+            Controls.Add(button9);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(button13);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(button5);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(txtResult);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtResult;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+        private Button button9;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
+        private Button button13;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button btn0;
+    }
+}
---
+++ MyScientific_Calculator/Form1.cs
@@ -0,0 +1,40 @@
+namespace MyScientific_Calculator
+{
+    public partial class Form1 : Form
+    {
+        double enterFirstValue, enterSecondValue;
+        String op;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EnterNumbers(object sender, EventArgs e)
+        {
+            Button num = (Button)sender;
+
+            if (txtResult.Text == "0")
+                txtResult.Text = "";
+        }
+    }
+}
